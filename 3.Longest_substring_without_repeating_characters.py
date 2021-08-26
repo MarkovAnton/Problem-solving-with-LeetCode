f33c214e4b2b024703c9a449c11edbc1f53490b8
MarkovAnton <markov_anton2001@mail.ru>
2021-08-26 10:22:07 +0300

    add 3.Longest_substring_without_repeating_characters.py

---
+++ 3.Longest_substring_without_repeating_characters.py
@@ -0,0 +1,19 @@
+'''
+Ссылка на описание задачи:
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
+'''
+
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        ans = 0
+        sub = ''
+        for char in s:
+            if char not in sub:
+                sub += char
+                ans = max(ans, len(sub))
+            else:
+                cut = sub.index(char)
+                sub = sub[cut+1:] + char
+        return ans
+
