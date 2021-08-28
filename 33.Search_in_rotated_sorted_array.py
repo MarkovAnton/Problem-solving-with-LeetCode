189e027232b5f4047132bf51d54caf0d0c3a1751
MarkovAnton <markov_anton2001@mail.ru>
2021-08-28 16:43:17 +0300

    add 33.Search_in_rotated_sorted_array.py

---
+++ 33.Search_in_rotated_sorted_array.py
@@ -0,0 +1,13 @@
+'''
+Ссылка на описание задачи:
+https://leetcode.com/problems/search-in-rotated-sorted-array/
+'''
+
+
+class Solution(object):
+    def search(self, nums, target):
+        if target in nums:
+            return nums.index(target)
+        else:
+            return -1
+
