5041371f69068ddb8ddc3983df564563f37539ae
MarkovAnton <markov_anton2001@mail.ru>
2021-08-28 16:05:39 +0300

    add 28.Implement_strStr.py

---
+++ 28.Implement_strStr.py
@@ -0,0 +1,10 @@
+'''
+Ссылка на описание задачи:
+https://leetcode.com/problems/implement-strstr/
+'''
+
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        return haystack.find(needle)
+
